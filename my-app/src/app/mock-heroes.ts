323510651b664de71d0fc9cba028c27e4834f825
Vadimochek <56549726+Vadimochek@users.noreply.github.com>
2022-07-01 15:04:43 +0300

    Tutorial 2 - Display list

---
+++ my-app/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero'
+
+export const HEROES: Hero[] = [
+  {id: 11, name: "Leroy", game: "Warcraft"},
+  {id: 22, name: "Red", game: "Angry Birds"},
+  {id: 33, name: "Pudge", game: "Dota 2"},
+  {id: 44, name: "Mario", game: "Mario"},
+  {id: 55, name: "Connor", game: "Detroit"}
+]
