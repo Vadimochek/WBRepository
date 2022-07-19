63ebda5aede609aa8286d35cbf2153e84aef6879
Vadimochek <56549726+Vadimochek@users.noreply.github.com>
2022-07-19 18:51:04 +0300

    Add navigate after google sign

--- book/src/app/services/auth.service.ts
+++ book/src/app/services/auth.service.ts
@@ -62,11 +62,9 @@ export class AuthService {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential!.accessToken;
         const user = result.user;
+        this.route.navigate(['book'])
       }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.customData.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
+console.log(error)
     });
   }
 }
